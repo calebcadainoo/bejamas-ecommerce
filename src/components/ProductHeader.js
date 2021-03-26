32bd4f54e2ccc632fd76d4505c9ca2401034dff3
Caleb Crosy Adaioo <calcronoo@gmail.com>
2021-03-26 04:04:23 +0000

    feat: product summary ui setup

--- src/components/ProductHeader.js
+++ src/components/ProductHeader.js
@@ -7,10 +7,10 @@ function ProductHeader() {
     <header className="product-header">
       {/* product title */}
       <div className="product-title flex">
-        <div className="product-name">
+        <div className="app-title">
           Samurai King Resting
         </div>
-        <button class="btn product-title-btn hide-on-mobile">Add to Cart</button>
+        <button className="btn product-title-btn hide-on-mobile">Add to Cart</button>
       </div>
 
       {/* image holder */}
@@ -20,9 +20,33 @@ function ProductHeader() {
           Photo of The Day
         </div>
       </div>
-      <button class="btn product-title-btn show-on-mobile">Add to Cart</button>
+      <button className="btn product-title-btn show-on-mobile">Add to Cart</button>
 
       {/* description */}
+      <div className="product-summary-box flex">
+        <div className="product-description-box">
+          <div className="app-title pad-y-15">
+            About The Samurai King Resting
+          </div>
+          <div className="product-category pad-y-7">Pets</div>
+          <p className="product-description-text">
+            So how did the classical Latin become so incoherent? According 
+            to McClintock, a 15th century typesetter likely scrambled part 
+            of Cicero's De Finibus in order to provide placeholder text to 
+            mockup various fonts for a type specimen book.
+          </p>
+        </div>
+
+        <div className="product-recommend-box text-right">
+          <div className="app-title pad-y-15">
+            People also buy
+          </div>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic necessitatibus dignissimos odio, ratione, harum suscipit accusantium esse laboriosam assumenda, itaque similique voluptatibus tenetur dolor quasi. Eaque ea ad magni nihil!
+          </p>
+        </div>
+      </div>
     </header>
   )
 }
