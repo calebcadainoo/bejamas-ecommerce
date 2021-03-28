9ff953969183769ff4ec4199cbd0c52511a9efdb
Caleb Crosy Adaioo <calcronoo@gmail.com>
2021-03-28 22:32:06 +0000

    feat: add hover animation for product item to show Add to Cart btn on hover

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -7,8 +7,8 @@ function ProductItem() {
       <div className="product-item-image">
         <img src={DogImage} alt="dog"/>
 				<div className="product-tag abs-top-left">Best Seller</div>
+			  <button className="btn product-cart-btn">Add to Cart</button>
       </div>
-			<button className="btn">Add to Cart</button>
 			<div className="light-50 pad-y-10">Pets</div>
 			<div className="heavy-text pad-y-7">Red Bench</div>
 			<div className="light-50">$34.55</div>
